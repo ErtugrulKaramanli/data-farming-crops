dec8d8621a2d833aaa9d70dfed49d0acc334f8ff
Ertuğrul Karamanlı <ertugrul.karamanli@gmail.com>
2026-07-15 13:18:33 +0300

    Completed Corn

--- farm/corn.py
+++ farm/corn.py
@@ -1,5 +1,12 @@
-
 class Corn:
-    # YOUR CODE HERE
-    pass
+    def __init__(self):
+        # grains isimli instance variable sıfırla başlatılır
+        self.grains = 0
 
+    def water(self):
+        # her çağrıldığında 10 tane grain ekler
+        self.grains += 10
+
+    def ripe(self):
+        # grain sayısı en az 15 ise True, değilse False döner
+        return self.grains >= 15
